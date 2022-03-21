951192c40eb909ca803d8b6da04eff24c13c179d
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-21 01:35:29 -0400

    delete endpoint written: /:id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -54,10 +54,15 @@ router.put('/:id', validateUserId, validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
+router.delete('/:id', validateUserId, async (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
-  console.log(req.user);
+  try {
+    await User.remove(req.params.id);
+    res.json(req.user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
