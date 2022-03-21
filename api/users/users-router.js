9b7ac4710ab04b2cdd0beebb290eee9eeddfd814
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-21 00:28:13 -0400

    written and implemented validatePost middleware

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 // The middleware functions also need to be required
 const {
-  validateUserId
+  validateUserId,
+  validateUser
 } = require('../middleware/middleware');
 
 const router = express.Router();
@@ -16,33 +17,40 @@ router.get('/', (req, res) => {
 router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  console.log(req.user);
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  console.log(req.name);
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user);
+  console.log(req.name);
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  console.log(req.user);
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  console.log(req.user);
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user);
 });
 
 // do not forget to export the router
