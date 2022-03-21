06d491169002a26bef7af462cbffa40fe0db878e
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-20 22:19:26 -0400

    middleware functions written

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,48 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
-}
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+};
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-}
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).json({
+      message: "user not found"
+    });
+  } else {
+    res.json(req.user);
+  }
+};
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-}
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({
+      message: "missing required name field"
+    });
+  }
+};
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-}
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({
+      message: "missing required text field"
+    });
+  }
+};
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+};
