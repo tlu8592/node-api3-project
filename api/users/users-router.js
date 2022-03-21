b9377e28495821f7ddd30a6077d1a8f51f8dbee5
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-20 22:06:52 -0400

    get endpoint written: /

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -10,6 +10,15 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Users information could not be retrieved"
+      })
+    })
 });
 
 router.get('/:id', (req, res) => {
